52aeace46fdb37bf34e8056386fd496a8e9abd7c
Антон Ивашкевич <tofcom99@gmail.com>
2023-10-15 18:39:47 +0300

    qq

---
+++ task 1.5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] A = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        int sum = 0;
+        bool add = true; // Флаг
+
+        foreach (int number in A)
+        {
+            if (add)
+            {
+                sum += number;
+            }
+            else
+            {
+                sum -= number;
+            }
+
+            add = !add; // Меняем флаг
+        }
+
+        Console.WriteLine("Сумма знакочередующейся последовательности: " + sum);
+    }
+}
