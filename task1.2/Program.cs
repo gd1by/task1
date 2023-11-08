573443e5750e202e617c3e2466b3a83969d0937f
gd1by <102508648+gd1by@users.noreply.github.com>
2023-11-08 21:22:18 +0200

    Update Program.cs

--- task1.2/Program.cs
+++ task1.2/Program.cs
@@ -1,29 +1,37 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 class Program
 {
+/*43.У вас есть доллары. Вы хотите обменять их на гривны.
+Есть информация о стоимости купли-продажи в банках города. 
+    В городе N банков. Составьте программу, определяющую, 
+    какой банк выбрать, чтобы выгодно обменять доллары на гривны.*/
     static void Main()
     {
+        // Створення списку банків з валютними курсами
         List<Bank> banks = new List<Bank>
         {
             new Bank("Bank 1", 27.50, 28.00),
             new Bank("Bank 2", 27.60, 28.10),
             new Bank("Bank 3", 27.45, 27.95),
-            // Добавьте другие банки
         };
 
-        double currentExchangeRate = 27.70;
-        double maxProfit = 0;
-        string bestBank = "";
+        double currentExchangeRate = 27.70; // Поточний обмінний курс
+        double maxProfit = 0; // Змінна для збереження максимальної вигоди
+        string bestBank = ""; // Змінна для збереження найкращого банку
 
+        // Перевіряємо кожен банк у списку
         foreach (var bank in banks)
         {
+            // Розраховуємо вигоду для кожного банку
             double profit = (bank.SellingRate - currentExchangeRate) * 100;
+
+            // Порівнюємо вигоду з максимальною вигодою
             if (profit > maxProfit)
             {
-                maxProfit = profit;
-                bestBank = bank.Name;
+                maxProfit = profit; // Оновлюємо максимальну вигоду
+                bestBank = bank.Name; // Запам'ятовуємо назву найкращого банку
             }
         }
 
